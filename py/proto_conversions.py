4afd7a470169dd45aa5ac0049117237235571abd
Stuart Berg <bergs@janelia.hhmi.org>
2013-11-19 14:26:51 -0500

    py: Fixed conversion functions, output to csv.

--- py/proto_conversions.py
+++ py/proto_conversions.py
@@ -6,34 +6,37 @@ from dvidmsg_pb2 import Array, DatasetDescription, Bounds
 
 conversion_spec = collections.namedtuple('conversion_spec', 'dvid_type proto_type numpy_type msg_field')
 
-conversion_specs = [ conversion_spec('i8',      'b',  numpy.int8,    'data8'),
-                     conversion_spec('u8',      'B',  numpy.uint8,   'data8'),
-                     conversion_spec('i16',     'h', numpy.int16,   'dataU32'),
-                     conversion_spec('u16',     'H', numpy.uint16,  'dataU32'),
-                     conversion_spec('i32',     int, numpy.int32,   'dataI32'),
-                     conversion_spec('u32',     long, numpy.uint32,  'dataU32'),
-                     conversion_spec('i64',     long, numpy.int64,   'dataI64'),
-                     conversion_spec('u64',     long, numpy.uint64,  'dataU64'),
-                     conversion_spec('float32', 'f', numpy.float32, 'dataFloat32'),
-                     conversion_spec('float64', 'd', numpy.float64, 'dataFloat64') ]
+conversion_specs = [ conversion_spec('i8',      'b',   numpy.int8,    'data8'),
+                     conversion_spec('u8',      'B',   numpy.uint8,   'data8'),
+                     conversion_spec('i16',     'h',   numpy.int16,   'dataI32'),
+                     conversion_spec('u16',     'H',   numpy.uint16,  'dataU32'),
+                     conversion_spec('i32',      int,  numpy.int32,   'dataI32'),
+                     conversion_spec('u32',      long, numpy.uint32,  'dataU32'),
+                     conversion_spec('i64',      long, numpy.int64,   'dataI64'),
+                     conversion_spec('u64',      long, numpy.uint64,  'dataU64'),
+                     conversion_spec('float32',  'f',  numpy.float32, 'dataFloat32'),
+                     conversion_spec('float64',  'd',  numpy.float64, 'dataFloat64') ]
 
 conversion_specs_from_dvid = { spec.dvid_type : spec for spec in conversion_specs }
 conversion_specs_from_numpy = { spec.numpy_type : spec for spec in conversion_specs }
 
 def convert_array_from_dvidmsg(msg):
-    _, _, dtype, msg_field = conversion_specs_from_dvid[msg.description.datatype]
+    dvid_type, _, dtype, msg_field = conversion_specs_from_dvid[msg.description.datatype]
     msg_data = getattr( msg.data, msg_field )
     
     shape = numpy.array(msg.description.bounds.stop) - msg.description.bounds.start
     assert numpy.prod(shape) == len(msg_data), \
         "Array from server has mismatched length and shape: {}, {}".format( shape, len(msg_data) )
 
-    # Use Fortran order under the hood, since that's the order that DVID gives us.
+    # Use Fortran order, since that's the DVID convention
     result = numpy.ndarray( shape, dtype=dtype, order='F' )
 
     # numpy arrays are always indexed in C-order, 
     #  so we have to transpose before assigning the flat data.
-    result.transpose().flat[:] = msg_data
+    if dvid_type == 'i8' or dvid_type == 'u8':
+        result.transpose().flat[:] = map(ord, msg_data)
+    else:
+        result.transpose().flat[:] = msg_data
     return result
 
 def convert_array_to_dvidmsg(a, dvid_start=None):
@@ -46,11 +49,15 @@ def convert_array_to_dvidmsg(a, dvid_start=None):
     dvid_type, proto_type, _, msg_field = conversion_specs_from_numpy[a.dtype.type]
     msg.description.datatype = dvid_type
 
-    # DVID expects fortran order, so transpose before flattening.
+    # We assume this array is provided in fortran order, because that's the dvid convention.
+    # But we need to transmit in C-order, so transpose() to get correct iteration order.
     flat_view = a.transpose().flat[:]
 
-    #print "Using field: ", msg_field, "proto_type: ", proto_type, "view type: ", flat_view.dtype
-    if isinstance( proto_type, str ):
+    if dvid_type == 'i8' or dvid_type == 'u8':
+        # protobuf uses strings for 8-bit types, so convert to string.
+        # There must be a better way of doing this than making a big copy like this...
+        setattr( msg.data, msg_field, "".join(map(chr, flat_view.view(numpy.uint8))) )
+    elif isinstance( proto_type, str ):
         getattr( msg.data, msg_field ).extend( array.array(proto_type, flat_view) )
     else:
         getattr( msg.data, msg_field ).extend( map(proto_type, flat_view) )
--- py/serialization_benchmark.py
+++ py/serialization_benchmark.py
@@ -6,44 +6,116 @@ from lazyflow.utility import Timer
 from dvidmsg_pb2 import Array
 import proto_conversions
 
-# Prepare array data
-send_data = numpy.random.random( (100,100,100) )
-send_data *= 100
-send_data = send_data.astype( numpy.int32 )
-data_mb = numpy.prod( send_data.shape ) * send_data.dtype.type().nbytes / 1e6
-print "Array data is {} MB".format( data_mb )
+class BenchmarkStats(object):
+    def __init__(self):
+        self.type_name = None
+        self.image_size_mb = None
+        self.message_size_mb = None
+        self.message_creation_seconds = None
+        self.serialization_seconds = None
+        self.deserialization_seconds = None
+        self.array_creation_seconds = None
 
-# Format into message
-with Timer() as timer:
-    send_msg = proto_conversions.convert_array_to_dvidmsg( send_data )
-print "Formatting message structure took {} seconds".format( timer.seconds() )
+def random_data( dtype, shape ):
+    if numpy.issubdtype(dtype, numpy.integer):
+        send_data = numpy.random.randint( numpy.iinfo(dtype).min/2, numpy.iinfo(dtype).max/2, shape )
+        send_data = send_data.astype(dtype)
+    else:
+        send_data = numpy.random.random( shape )
+        send_data = send_data.astype( dtype )
+    return send_data
 
-with contextlib.closing( StringIO.StringIO() ) as strbuf:
-    # Serialize it
+def run_benchmark( dtype, shape ):
+    stats = BenchmarkStats()
+    stats.type_name = proto_conversions.conversion_specs_from_numpy[ dtype ].dvid_type
+    stats.image_size_mb = proto_conversions.image_size_mb = numpy.prod( shape ) / 1e6
+
+    # Prepare array data
+    send_data = random_data( dtype, shape )
+    stats.image_size_mb = numpy.prod( send_data.shape ) * send_data.dtype.type().nbytes / 1e6
+    
+    # Format into message
     with Timer() as timer:
-        strbuf.write( send_msg.SerializeToString() )
-    print "Serialization took {} seconds".format( timer.seconds() )
+        send_msg = proto_conversions.convert_array_to_dvidmsg( send_data )
+    stats.message_creation_seconds = timer.seconds()
+    #print "Formatting message structure took {} seconds".format( timer.seconds() )
 
-    # Read the data into a string before we discard strbuf
-    strbuf.seek(0)
-    buffer_data = strbuf.read()
-    print "Serialized data is {} MB".format( len(buffer_data) / 1e6 )
+    with contextlib.closing( StringIO.StringIO() ) as strbuf:
+        # Serialize it
+        with Timer() as timer:
+            strbuf.write( send_msg.SerializeToString() )
+        stats.serialization_seconds = timer.seconds()
+        #print "Serialization took {} seconds".format( timer.seconds() )
+    
+        # Read the data into a string before we discard strbuf
+        strbuf.seek(0)
+        buffer_data = strbuf.read()
+        stats.message_size_mb = len(buffer_data) / 1e6
+        #print "Serialized data is {} MB".format( len(buffer_data) / 1e6 )
 
-# Deserialize
-rcv_msg = Array()
-with Timer() as timer:
-    rcv_msg.ParseFromString( buffer_data )
-print "Deserialization took {} seconds".format( timer.seconds() )
+    # Deserialize
+    rcv_msg = Array()
+    with Timer() as timer:
+        rcv_msg.ParseFromString( buffer_data )
+    stats.deserialization_seconds = timer.seconds()
+    #print "Deserialization took {} seconds".format( timer.seconds() )
+    
+    # Convert back to ndarray
+    with Timer() as timer:
+        rcv_data = proto_conversions.convert_array_from_dvidmsg( rcv_msg )
+    stats.array_creation_seconds = timer.seconds()
+    #print "Converting message structure to ndarray took {} seconds".format( timer.seconds() )
 
-# Convert back to ndarray
-with Timer() as timer:
-    rcv_data = proto_conversions.convert_array_from_dvidmsg( rcv_msg )
-print "Converting message structure to ndarray took {} seconds".format( timer.seconds() )
+    # Check the results.
+    #print "Checking for errors..."
+    assert rcv_msg.description == send_msg.description, "Message description was not echoed correctly."
+    assert (rcv_data == send_data).all(), "Data mismatch after conversion"
 
-# Check the results.
-print "Checking for errors..."
-assert rcv_msg.description == send_msg.description, "Message description was not echoed correctly."
-assert (rcv_data == send_data).all(), "Data mismatch after conversion"
+    #print "Successful round-trip (de)serialization!"
+    return stats
 
-print "Successful round-trip (de)serialization!"
+def print_stats_header():
+    header = "image size,"\
+             "dtype,"\
+             "encoded message size,"\
+             "message creation time,"\
+             "serialization time,"\
+             "deserialization time,"\
+             "array creation time"
+    print header
 
+def print_stats( stats ):
+    stats_row = ( "{image_size_mb},"
+                  "{type_name},"
+                  "{message_size_mb},"
+                  "{message_creation_seconds},"
+                  "{serialization_seconds},"
+                  "{deserialization_seconds},"
+                  "{array_creation_seconds}"
+                  "".format( **stats.__dict__ ) )
+    print stats_row
+
+def main():
+    shapes = [ (100,100),
+               (100,1000),
+               (1000,1000),
+               (10,1000,1000),
+               (100,1000,1000),
+               (1000,1000,1000) ]
+    
+    print_stats_header()
+    
+    dtypes = [ numpy.uint8, numpy.int8,
+               numpy.uint16, numpy.int16,
+               numpy.uint32, numpy.int32,
+               numpy.uint64, numpy.int64,
+               numpy.float32, numpy.float64 ]
+    for shape in shapes:
+        for dtype in dtypes:
+            print_stats( run_benchmark( dtype, shape ) )
+    
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    sys.exit( main() )
